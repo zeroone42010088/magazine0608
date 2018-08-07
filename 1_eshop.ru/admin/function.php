a3cadba2e3889b7d0eabd521273bc425cc5b8f1a
Глеб <stroikagoda@yandex.ru>
2018-08-07 17:56:48 +0400

    Пример магазина 8 занятие

---
+++ 1_eshop.ru/admin/function.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eshop";
+
+function connect(){
+    $conn = mysqli_connect("localhost", "root", "root", "eshop");
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    return $conn;
+}
+
+function init(){
+    //вывожу список товаров
+    $conn = connect();
+    $sql = "SELECT * FROM goods";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $out = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $out[$row["id"]] = $row;
+        }
+        echo json_encode($out);
+    } else {
+        echo "0";
+    }
+    mysqli_close($conn);
+}
---
+++ 1_eshop.ru/goods.json
@@ -0,0 +1,30 @@
+{
+  "1234": {
+    "name": "Яблоки",
+    "cost": 44.2,
+    "description": "Сочные спелы",
+    "order": 3,
+    "img" : "apple.png"
+  },
+  "1235": {
+    "name": "Вишня",
+    "cost": 54.6,
+    "description": "Кислая красная",
+    "order": 5,
+    "img" : "cherry.png"
+  },
+  "1236": {
+    "name": "Виноград",
+    "cost": 94.2,
+    "description": "Сладкий, синий и зеленый",
+    "order": 1,
+    "img" : "grape.png"
+  },
+  "1237": {
+    "name": "Арбуз",
+    "cost": 23,
+    "description": "Сладкий красный",
+    "order": 8,
+    "img" : "watermelon.png"
+  }
+}
